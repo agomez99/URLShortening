95276991927a2b05fdbdc3e5317d55b2c696c0f7
Austine Gomez <austine.gomez@yahoo.com>
2023-06-04 23:02:18 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import Image from 'next/image';
 
 export default function Home() {
@@ -23,39 +23,77 @@ export default function Home() {
           </p>
           <Button variant="primary">Get Started</Button>
           <div>
-          <Image src="/images/illustration-working.svg" alt="working" width={600} height={450} />
+            <Image src="/images/illustration-working.svg" alt="working" width={600} height={450} />
           </div>
         </main>
         <div className={styles.linkinput}>
-          <input type="text" placeholder="Enter your link" id="linkinput"  />
+          <input type="text" placeholder="Enter your link" id="linkinput" />
           <Button variant="primary">Shorten It!</Button>
         </div>
 
-        <footer className={styles.footer}>
+        <div className={styles.statistics}>
+          <h1>Advance statistics</h1>
+          <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Brand Recognition</Card.Title>
+              <Card.Text>
+                Boost your brand recognition with each click. Generic links don’t
+                mean a thing. Branded links help instil confidence in your content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Detailed Records</Card.Title>
+              <Card.Text>
+                Gain insights into who is clicking your links. Knowing when and where
+                people engage with your content helps inform better decisions.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Fully Customizable</Card.Title>
+              <Card.Text>
+                Improve brand awareness and content discoverability through customizable
+                links, supercharging audience engagement.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+
+
+        <div className={styles.bottom}>
           Boost your links today
-
-          Get Started
-
-          Features
-
-          Link Shortening
-          Branded Links
-          Analytics
-
-          Resources
-
-          Blog
-          Developers
-          Support
-
-          Company
-
-          About
-          Our Team
-          Careers
-          Contact
-        </footer>
+        </div>
+        <Button variant="primary" >Get Started</Button>
       </div>
+      <footer className={styles.footer}>
+
+
+        Features
+
+        Link Shortening
+        Branded Links
+        Analytics
+
+        Resources
+
+        Blog
+        Developers
+        Support
+
+        Company
+
+        About
+        Our Team
+        Careers
+        Contact
+      </footer>
     </>
   );
 }
